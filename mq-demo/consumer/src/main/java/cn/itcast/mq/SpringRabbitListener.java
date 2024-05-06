a4487057b68d88fdfaaf2e7b772a6b521f92a69f
xxl <1628805655@qq.com>
2024-05-06 23:37:23 +0800

    SpringAMQP实现workqueue

--- mq-demo/consumer/src/main/java/cn/itcast/mq/SpringRabbitListener.java
+++ mq-demo/consumer/src/main/java/cn/itcast/mq/SpringRabbitListener.java
@@ -3,10 +3,34 @@ package cn.itcast.mq;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+
 @Component
 public class SpringRabbitListener {
+    /**
+     * 监听简单队列
+     *
+     * @param message 消息
+     */
     @RabbitListener(queues = "simple.queue")
-    public void listen(String message) {
+    public void listenSimpleQueue(String message) {
         System.out.println("接收到simple.queue消息：" + message);
+    }
+
+    /**
+     * 监听工作队列
+     *
+     * @param message 消息
+     */
+    @RabbitListener(queues = "work.queue")
+    public void listenWorkQueue1(String message) throws InterruptedException {
+        System.out.println("消费者1接收到work.queue消息：" + message + "  time：" + LocalTime.now());
+        Thread.sleep(20);
+    }
+
+    @RabbitListener(queues = "work.queue")
+    public void listenWorkQueue2(String message) throws InterruptedException {
+        System.err.println("消费者2。。。接收到work.queue消息：" + message + "  time：" + LocalTime.now());
+        Thread.sleep(200);
     }
 }
--- mq-demo/publisher/src/test/java/cn/itcast/mq/helloworld/SpringAmqpTest.java
+++ mq-demo/publisher/src/test/java/cn/itcast/mq/helloworld/SpringAmqpTest.java
@@ -2,10 +2,14 @@ package cn.itcast.mq.helloworld;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,11 +17,31 @@ public class SpringAmqpTest {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    /**
+     * 测试发送消息到简单队列
+     */
     @Test
-    public void testSendMessage() {
+    public void testSendMessageToSimpleQueue() {
         String message = "hello, spring amqp!";
         rabbitTemplate.convertAndSend("simple.queue", message);
         System.out.println("发送消息成功：【" + message + "】");
     }
 
+    @Resource
+    private RabbitAdmin rabbitAdmin;
+
+    /**
+     * 测试发送消息到工作队列
+     */
+    @Test
+    public void testSendMessageToWorkQueue() {
+        // 创建队列
+        Queue queue = new Queue("work.queue", true, false, false);
+        rabbitAdmin.declareQueue(queue);
+        for (int i = 1; i <= 50; i++) {
+            String message = "hello, spring amqp!————" + i;
+            rabbitTemplate.convertAndSend("work.queue", message);
+            System.out.println("发送消息成功：【" + message + "】");
+        }
+    }
 }
